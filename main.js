a199c49f8ffb01ce7a516d07d022b59c209f5e24
ElisaTorelli <elisa.torelli1@gmail.com>
2022-05-24 16:17:09 +0200

    add comments

--- main.js
+++ main.js
@@ -1,21 +1,15 @@
-// Il prezzo del biglietto ha un costo di 0.21€ per km da percorrere.
-// Se il passeggerò è minorenne, il prezzo del biglietto sarà scontato del 20%.
-// Se l'età del passeggerò è un valore maggiore o uguale di 65, il prezzo del biglietto sarà scontato del 40%.
-// Se il valore inserito nel prompt non è un valore numerico, dovrà essere restituito a video 
-//il seguente messaggio: "il valore inserito non è un numero. Riprova"
-
-
-
 //  ASK FOR USER INPUT
 let userInput = parseInt(prompt("Enter number of km to be covered: "));
 let userAge = parseInt(prompt("Enter age of the passenger: "));
 let price = userInput * 0.21;
 
-
+//IF USER INSERTS A VALUE != FROM NUMBER RETURN AN ALERT
 if (userInput == (isNaN) || userAge == (isNaN)){
     alert("The inserted value is not valid! Try Again!")
 }
 
+//INSERT DIFFERENT CONDITIONS FOR DIFFERENT CASES
 price = userAge < 18 ? price * 0.8 : userAge >= 65 ? price * 0.6 : price
 
+//GET RESULT
 document.getElementById("main-container").innerHTML = "Your ticket's price is: ${Math.ceil(price)}";
